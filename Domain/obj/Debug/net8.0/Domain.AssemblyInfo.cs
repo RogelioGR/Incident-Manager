46d271a772fa5ebd0cf6bfabe215c31a1f12bd39
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2025-02-02 16:39:45 -0500

    Reemplazando proyecto

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a4bb049c52ec4bdad8d79a8e29223d62ea509e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApiIncidentManager/Program.cs
+++ WebApiIncidentManager/Program.cs
@@ -12,7 +12,62 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+/* 
+var builder = WebApplication.CreateBuilder(args);
 
+// Agregar servicios al contenedor
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registro de servicios personalizados
+builder.Services.AddScoped<IRolesServices, RolesServices>();
+builder.Services.AddScoped<IDepartamentoServices, DepartamentoServices>();
+builder.Services.AddScoped<IPrioridadServices, PrioridadServices>();
+builder.Services.AddScoped<IEstadosReportesServices, EstadosReportesServices>();
+builder.Services.AddScoped<IUsuarioServices, UsuarioServices>();
+builder.Services.AddScoped<IReportesServices, ReportesServices>();
+
+
+
+
+builder.Services.AddDbContext<SistemaKempinskiContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DBConnection"),
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql")
+    ));
+
+// Configuración de CORS (si es necesario)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+    );
+});
+
+// Construir la aplicación
+var app = builder.Build();
+
+// Configuración del pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// Middleware de CORS (opcional)
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+ 
+ 
+ */
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
