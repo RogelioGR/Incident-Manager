d42dfb12f6e4085f2b24ed2338c7f616fadab52d
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2025-02-02 23:20:08 -0500

    Implementar las funciones de Crud de las tablas en el Banckend

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa768a77f4ec658c61a2db0f781dca7ca1f02046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06084ccea952c10d61937415e5d440a86ab5c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
