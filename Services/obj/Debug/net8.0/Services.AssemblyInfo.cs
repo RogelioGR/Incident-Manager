c028a005436576828659abcd1075cc88390dd948
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2025-02-05 01:27:41 -0500

    se Implemnto la funcion de visualizar Usuarios

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce558899d2f9df976187b6422d816e636af3ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23edf320ab8a7dac6ab3f48d13cf59f184a96244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
